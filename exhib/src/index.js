c5eb75a26815587d4071d3676a7211289266bcc6
Stephpp <stephenpapp30@gmail.com>
2023-03-14 19:14:14 +0200

    Initial commit, project setup

---
+++ exhib/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './static/css/main.css';
+import './static/css/bootstrap.min.css'
+import App from './App';
+import Nav from './layout/Nav'
+import Footer from './layout/Footer'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Nav/>
+    <App />
+    <Footer/>
+  </React.StrictMode>
+);
