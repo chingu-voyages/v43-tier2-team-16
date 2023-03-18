80f43387c658148688b8088c75f98c0a85e68603
Stephpp <stephenpapp30@gmail.com>
2023-03-18 21:03:40 +0200

    Add missing import

--- exhib/src/index.js
+++ exhib/src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import Nav from './layout/Nav'
 import Footer from './layout/Footer'
 import { BrowserRouter } from "react-router-dom"
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
