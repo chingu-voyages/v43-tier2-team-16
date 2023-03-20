d2a1d3435e77abce75a2d16a3fcdf6a3f3f49e36
Abdulramon Lasisi <lasisiolalekan081@gmail.com>
2023-03-20 09:33:33 +0100

    test

--- exhib/src/App.js
+++ exhib/src/App.js
@@ -1,30 +1,30 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/LandingPage/Home";
-import About from "./pages/AboutPage/About";
-import Project from "./pages/Projects/Project";
-import Login from "./pages/Profile/Login";
-import Signup from "./pages/Profile/Signup";
-import Profile from "./pages/Profile/Profile";
-import WithoutNav from "./layout/WithoutNav";
-import WithNav from "./layout/WithNav";
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/LandingPage/Home'
+import About from './pages/AboutPage/About'
+import Project from './pages/Projects/Project'
+import Login from './pages/Profile/Login'
+import Signup from './pages/Profile/Signup'
+import Profile from './pages/Profile/Profile'
+import WithoutNav from './layout/WithoutNav'
+import WithNav from './layout/WithNav'
 
 function App() {
-   return (
-      <>
-         <Routes>
-            <Route element={<WithoutNav />}>
-               <Route path="/login" element={<Login />} />
-               <Route path="/signup" element={<Signup />} />
-            </Route>
-            <Route element={<WithNav />}>
-               <Route path="/" element={<Home />} />
-               <Route path="/about" element={<About />} />
-               <Route path="/project" element={<Project />} />
-               <Route path="/profile" element={<Profile />} />
-            </Route>
-         </Routes>
-      </>
-   );
+  return (
+    <>
+      <Routes>
+        <Route element={<WithoutNav />}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Route>
+        <Route element={<WithNav />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/project' element={<Project />} />
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
