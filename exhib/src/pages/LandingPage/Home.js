7f516a3b46deef924a3bc1518095a8d317d69ba9
preye omusuku <omusukup@yahoo.com>
2023-03-21 12:47:06 +0100

    updated home

--- exhib/src/pages/LandingPage/Home.js
+++ exhib/src/pages/LandingPage/Home.js
@@ -1,11 +1,9 @@
-
 import classes from '../LandingPage/home.module.css'
 
 function Home() {
     return (
       <div className={classes.home}>
         <h1>Home Page</h1>
-       
       </div>
     );
   }
